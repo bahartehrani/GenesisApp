7201b42fa8f7e663febe187ff371a43892c86608
Bahar Tehranipoor <tehranipoorbp@gmail.com>
2020-07-02 15:38:30 -0400

    Initial Commit

---
+++ GenesisApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GenesisApp
+//
+//  Created by Productivity on 7/2/20.
+//  Copyright © 2020 Genesis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
